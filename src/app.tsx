c82480973d81f903adc8fd9cface17dd4c24ec8e
yutaroadachi <ytr_adc72@yahoo.co.jp>
2021-11-22 20:35:27 +0900

    feat: ロジックを実装

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,8 @@
+import { useCallback, useState } from "preact/hooks";
+
 export const App = () => {
+  const { input, onChangeInput, result, shuffle } = useApp();
+
   return (
     <div className="flex flex-col min-h-screen bg-gray-100 text-purple-600">
       <header className="py-4">
@@ -17,13 +21,18 @@ export const App = () => {
               <br />
               <textarea
                 id="input"
+                value={input}
+                onChange={onChangeInput}
                 autofocus
                 rows={10}
                 className="w-80 p-4 mt-2"
               />
             </div>
-            <button className="bg-purple-600 hover:bg-purple-500 font-bold text-white px-4 py-2 border-b-4 hover:border-b-0 border-purple-900 hover:border-purple-800 rounded">
-              実行
+            <button
+              onClick={() => shuffle(input)}
+              className="bg-purple-600 hover:bg-purple-500 font-bold text-white px-4 py-2 border-b-4 hover:border-b-0 border-purple-900 hover:border-purple-800 rounded"
+            >
+              {result ? "再実行" : "実行"}
             </button>
             <div className="text-right">
               <label for="result" className="font-bold">
@@ -32,18 +41,19 @@ export const App = () => {
               <br />
               <textarea
                 id="result"
+                value={result}
                 readonly
                 rows={10}
                 className="w-80 p-4 mt-2"
-              >
-                hoge
-              </textarea>
+              />
             </div>
           </div>
           <div className="flex justify-end mt-2">
-            <button className="text-sm text-purple-600 hover:text-purple-300 border-b border-purple-600 hover:border-purple-300">
-              コピー
-            </button>
+            {result && (
+              <button className="text-sm text-purple-600 hover:text-purple-300 border-b border-purple-600 hover:border-purple-300">
+                コピー
+              </button>
+            )}
           </div>
         </div>
       </main>
@@ -54,4 +64,37 @@ export const App = () => {
       </footer>
     </div>
   );
+};
+
+const useApp = () => {
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState("");
+  const onChangeInput = useCallback(
+    (e: any) => {
+      setInput(e.target.value);
+    },
+    [setInput]
+  );
+  const shuffle = useCallback(
+    (input: string) => {
+      const inputArray = input.split(/\r\n|\n/);
+      const shuffledArray = shuffleArray(inputArray);
+      const result = shuffledArray.join("\n");
+      setResult(result);
+    },
+    [setResult]
+  );
+
+  return { input, onChangeInput, result, shuffle };
+};
+
+/** @see https://qiita.com/pure-adachi/items/77fdf665ff6e5ea22128#%E3%83%80%E3%82%B9%E3%83%86%E3%83%B3%E3%83%95%E3%82%A7%E3%83%AB%E3%83%89%E3%81%AE%E6%89%8B%E6%B3%95 */
+const shuffleArray = <T extends any>(array: T[]) => {
+  let copiedArray = [...array];
+  for (let i = copiedArray.length; i > 1; i--) {
+    const k = Math.floor(Math.random() * i);
+    [copiedArray[k], copiedArray[i - 1]] = [copiedArray[i - 1], copiedArray[k]];
+  }
+
+  return copiedArray;
 };
